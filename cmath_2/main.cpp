9ac8b2f2b082c62221cf19d928341e98f9d5b816
Rowan Ferrabee <rowanferrabee@Rowans-MacBook-Pro.local>
2014-03-25 16:19:55 -0400

    Initial Commit

---
+++ cmath_2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cmath_2
+//
+//  Created by Rowan Ferrabee on 2014-03-25.
+//  Copyright (c) 2014 Rowan Ferrabee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
