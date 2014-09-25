65c3466acb440e5318ba61aa2a90ada7f17167ec
RowanFerrabee <rowan.ferrabee@gmail.com>
2014-09-25 19:41:22 -0400

    Update main.cpp

--- cmath_2/main.cpp
+++ cmath_2/main.cpp
@@ -6,51 +6,15 @@
 
 using namespace std;
 
-//class Shape{
-//    
-//public:
-//    virtual void print()=0;
-//    virtual void printShapeName()=0;
-//    virtual double area();
-//    virtual double volume();
-//protected:
-//    string name;
-//    
-//};
-//
-//void Shape::printShapeName(){
-//
-//    cout<<name<<endl;
-//
-//}
-//
-//class Point: public Shape{
-//
-//public:
-//    Point(int a=0, int b=0){x=a;y=b;}
-//    double distance(){return sqrt(x*x+y*y);}
-//    void printPoint(){cout<<x<<","<<y<<endl;}
-//    
-//protected:
-//    int x;
-//    int y;
-//
-//};
-//
-//class Circle: public Point{
-//
-//public:
-//    Circle(int r){radius = r; name = "circle";}
-//    void setPoint(int a, int b){x=a;y=b;}
-//    void printCircle(){cout<<"(x - "<<x<<")^2 +(y - "<<y<<")^2 = "<<radius<<"^2"<<endl;}
-//    double area(){return 3.1415*radius*radius;}
-//private:
-//    int radius;
-//};
-
 int main(){
     
-        
+    Fraction f1(1,2), f2(10/9), f3(2);
+    ComplexNum n1(0,0), n2(1,-1);
+    RomanNum r1(19), r2("XVII");
+    
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    Array a1(arr);
+    Combinatorics c1(arr);
     
 }
  
